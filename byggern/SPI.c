855ca68eebab43effa46d16609a620447e075e82
Sander Furre <sanderfu@ntnu.no>
2019-09-25 11:48:18 +0200

    changes to spi

--- byggern/SPI.c
+++ byggern/SPI.c
@@ -11,21 +11,21 @@
 void SPI_MasterInit(void)
 {
 	//select slave output
-	DDRB = (1 << PB4);
+	DDRB |= (1 << PB4);
 	/* Set MOSI and SCK output, all others input */
-	DDRB = (1 << PB5) | (1 << PB7);
+	DDRB |= (1 << PB5) | (1 << PB7);
 	/* Enable SPI, Master, set clock rate fck/16 */
-	SPCR = (1 << SPE) | (1 << MSTR) | (1 << SPR0);
+	SPCR |= (1 << SPE) | (1 << MSTR) | (1 << SPR0);
 }
 
 void SPI_MasterTransmit(char cData)
 {
 	//select slave low
-	PORTB = (0 << PB4);
+	PORTB |= (0 << PB4);
 	/* Start transmission */
 	SPDR = cData;
 	/* Wait for transmission complete */
 	while (!(SPSR & (1<<SPIF)));
 	//select slave high
-	PORTB = (1 << PB4);
+	PORTB |= (1 << PB4);
 }
--- byggern/byggern.c
+++ byggern/byggern.c
@@ -4,7 +4,14 @@
  * Created: 30.08.2019 15:12:42
  *  Author: torsteoe
  */ 
+#include <avr/io.h>
+#include <stdio.h>
+#include <stdlib.h>
 
+
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include "menu.h"
 
 #include "setup.h"
 
@@ -27,16 +34,18 @@ int main(void)
 {
 	setupInit();
 	test_SRAM();
-	
+	SPI_MasterTransmit(0xAA);
+	SPI_MasterTransmit(0x01);
+	printf("%i",SPDR);
 
 	while (1) {
 		//Put microcontroller to sleep until next interrupt. 
-		sleep_now();
+		//sleep_now();
 		                         
 		
 		
-		test_resetMenu();
-		test_outputControllers(joystick_pos, slider_pos, buttons);		
+		//test_resetMenu();
+		//test_outputControllers(joystick_pos, slider_pos, buttons);		
 		//_delay_ms(50);	
 	}
 }
