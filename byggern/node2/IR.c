45a4ab9e6c94a4e734bc6221aeeb077c27b12f35
torsteoe <torsteoe@stud.ntnu.no>
2019-10-18 15:25:04 +0200

    start ir

---
+++ byggern/node2/IR.c
@@ -0,0 +1,6 @@
+/*
+ * IR.c
+ *
+ * Created: 18.10.2019 14:20:46
+ *  Author: torsteoe
+ */ 
---
+++ byggern/node2/IR.h
@@ -0,0 +1,16 @@
+/*
+ * IR.h
+ *
+ * Created: 18.10.2019 14:20:57
+ *  Author: torsteoe
+ */ 
+
+
+#ifndef IR_H_
+#define IR_H_
+
+
+
+
+
+#endif /* IR_H_ */
