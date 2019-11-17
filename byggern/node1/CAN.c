31ea2a0dd18859f2acdceb6417e74dc3893ef9e0
Torstein Ørbeck Eliassen <torsteoe@ntnu.no>
2019-11-17 15:44:21 +0100

    remove unnecessary receive buffer

--- byggern/node1/CAN.c
+++ byggern/node1/CAN.c
@@ -19,41 +19,33 @@
 
 void CAN_clearInterrupt()
 {
-	//CLearing interrupt
 	uint8_t mask = 0b11;
 	CAN_controller_bitModify(mask, CANINTF, 0b00);
 }
 void CAN_transmit_message(CAN_message_t *message) {
-	//printf("Transmitting message");
-	static int buffer_number = 0;
 	
+	//changing between three buffers
+	static int buffer_number = 0;
 	buffer_number++;
 	buffer_number = buffer_number%3;
 	
-	//Load id in registers TXBnSIDH and TXBnSIDL
+	//Load ID in message
 	switch(buffer_number) {
 			case 0:
 				CAN_controller_bitModify(0b11100000, TXB0SIDL, (uint8_t) ((message->ID & 0b111) << 5));
 				CAN_controller_write(TXB0SIDH, (uint8_t) ( message->ID >> 3) );
-				
-				//printf("Sending whole id: %i", message->ID );
-
-
 				break;
 			case 1:
 				CAN_controller_bitModify(0b11100000, TXB1SIDL, (uint8_t) ((message->ID & 0b111) << 5));
-				CAN_controller_write(TXB1SIDH, (uint8_t) ( message->ID >> 3) );
-				
+				CAN_controller_write(TXB1SIDH, (uint8_t) ( message->ID >> 3) );				
 				break;
 			case 2:
 				CAN_controller_bitModify(0b11100000, TXB2SIDL, (uint8_t) ((message->ID & 0b111) << 5));
 				CAN_controller_write(TXB2SIDH, (uint8_t) ( message->ID >> 3) );
 				break;
-			
-			
 	}
 		
-	//Load length in register TXBnDLC
+	//Filling message with data length
 	switch(buffer_number) {
 			case 0:
 				CAN_controller_bitModify(0b1111, TXB0DLC, message->data_length);
@@ -64,11 +56,10 @@ void CAN_transmit_message(CAN_message_t *message) {
 			case 2:
 				CAN_controller_bitModify(0b1111, TXB2DLC, message->data_length);
 				break;
-			
-			
+				
 	}
 
-	//iterate in for loop length = message.length and load TXBnDm
+	//Filling message with data.
 	for (uint8_t i = 0; i != message->data_length; i++) {
 		switch(buffer_number) {
 			case 0:
@@ -81,8 +72,6 @@ void CAN_transmit_message(CAN_message_t *message) {
 			case 2:
 				CAN_controller_write(TXB2D0 + i, message->data[i]);
 				break;
-			
-			
 		}
 		
 	}
@@ -93,41 +82,17 @@ void CAN_transmit_message(CAN_message_t *message) {
 
 	CAN_controller_RTS(buffer_number);
 	
-	//ERROR HANDLING?	
+	//Currently no error handling	
 }
 
 void CAN_receiveMessage(volatile CAN_message_t * received_message) {
 	
 	
-	uint8_t buffer = 0;
-	switch (buffer) {
-		case 0:
-				received_message->ID = ((CAN_controller_read(MCP_RXB0SIDL) & 0b11100000) >> 5) + (CAN_controller_read(MCP_RXB0SIDH) << 3); 
-				
-				//printf("Whole id: %i\n\r", received_message->ID);
-				received_message->data_length = (CAN_controller_read(MCP_RXB0DLC) & 0b00001111);
-				//printf("data length read: %i\n\r",(CAN_controller_read(MCP_RXB0DLC) & 0b00001111));
-				for (uint8_t i = 0; i != received_message->data_length; i++) {
-					received_message->data[i] = CAN_controller_read(MCP_RXB0D0 + i);
-					//printf("data: %i\t", received_message->data[i]);
-
-				}
-				break;
-		case 1:
-				received_message->ID = ((CAN_controller_read(MCP_RXB1SIDL) & 0b11100000) >> 5) + (CAN_controller_read(MCP_RXB1SIDH) << 3); 
-				
-				printf("Whole id: %i\n\r", received_message->ID);
-				received_message->data_length = (CAN_controller_read(MCP_RXB1DLC) & 0b00001111);
-				printf("data length read: %i\n\r",(CAN_controller_read(MCP_RXB1DLC) & 0b00001111));
-				for (uint8_t i = 0; i != received_message->data_length; i++) {
-					received_message->data[i] = CAN_controller_read(MCP_RXB1D0 + i);
-					printf("data: %i\t", received_message->data[i]);
-
-				}
-				break;
+	received_message->ID = ((CAN_controller_read(MCP_RXB0SIDL) & 0b11100000) >> 5) + (CAN_controller_read(MCP_RXB0SIDH) << 3); 
+	received_message->data_length = (CAN_controller_read(MCP_RXB0DLC) & 0b00001111);
+	for (uint8_t i = 0; i != received_message->data_length; i++) {
+		received_message->data[i] = CAN_controller_read(MCP_RXB0D0 + i);
 	}
-	
-	
 	
 	
 }
